06a930827435e39345bf8faa6885a56e1c3e73c5
Sirius198 <mtwbp101@gmail.com>
2022-11-21 06:57:58 +0200

    d

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <v-header id="header">
       <div class="flex_contheader">
         <div class="statusheaderleft">
-          <img class="mainimglogo" src="../assets/img/cheqd-Logo-White.png" alt="logo"/>
+          <img class="mainimglogo" src="../assets/img/logo.png" alt="logo"/>
         </div>
         <h1 class="testnetheader">Testnet Faucet</h1>
         <div class="statusheader">
@@ -119,7 +119,7 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer
+    <!-- <v-footer
       class="pagefooter"
       relative
     >
@@ -130,7 +130,7 @@
         <a href="https://github.com/cheqd" class="social-links__icon" target="_blank"><span class="social-links__icon"><svg version="1.0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 480 512" aria-labelledby="github-alt4" role="img"><title id="github-alt4">Follow us on Facebook</title><g id="github-alt4_layer"><path d="M186.1 328.7c0 20.9-10.9 55.1-36.7 55.1s-36.7-34.2-36.7-55.1 10.9-55.1 36.7-55.1 36.7 34.2 36.7 55.1zM480 278.2c0 31.9-3.2 65.7-17.5 95-37.9 76.6-142.1 74.8-216.7 74.8-75.8 0-186.2 2.7-225.6-74.8-14.6-29-20.2-63.1-20.2-95 0-41.9 13.9-81.5 41.5-113.6-5.2-15.8-7.7-32.4-7.7-48.8 0-21.5 4.9-32.3 14.6-51.8 45.3 0 74.3 9 108.8 36 29-6.9 58.8-10 88.7-10 27 0 54.2 2.9 80.4 9.2 34-26.7 63-35.2 107.8-35.2 9.8 19.5 14.6 30.3 14.6 51.8 0 16.4-2.6 32.7-7.7 48.2 27.5 32.4 39 72.3 39 114.2zm-64.3 50.5c0-43.9-26.7-82.6-73.5-82.6-18.9 0-37 3.4-56 6-14.9 2.3-29.8 3.2-45.1 3.2-15.2 0-30.1-.9-45.1-3.2-18.7-2.6-37-6-56-6-46.8 0-73.5 38.7-73.5 82.6 0 87.8 80.4 101.3 150.4 101.3h48.2c70.3 0 150.6-13.4 150.6-101.3zm-82.6-55.1c-25.8 0-36.7 34.2-36.7 55.1s10.9 55.1 36.7 55.1 36.7-34.2 36.7-55.1-10.9-55.1-36.7-55.1z"></path></g></svg></span></a>
       </div>
       <p style="text-align: center; font-size: 18px;">&copy; {{ new Date().getFullYear() }} cheqd - The network for authentic data | <a href="https://www.cheqd.io/privacy-policy?hsLang=en" rel="noopener" target="_blank">Privacy policy</a></p>
-    </v-footer>
+    </v-footer> -->
   </v-app>
 </template>
 
@@ -145,8 +145,8 @@ export default {
       title: 'BLU Testnet faucet',
       faucet_status: '',
       faucet_status_color: 'green',
-      items: ['cheqd-testnet-4'],
-      selected: 'cheqd-testnet-4',
+      items: ['BLU'],
+      selected: 'BLU',
       dialog: false
     }
   },
